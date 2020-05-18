2225a8404bcd704cd2287814b89354fd6800699e
tquang14 <tquang14@users.noreply.github.com>
2020-05-18 10:07:41 +0700

    update UI and feature: add new tree

--- web_dieuKhien_chiCoUI/js/add_optional_info/add_Info.js
+++ web_dieuKhien_chiCoUI/js/add_optional_info/add_Info.js
@@ -13,3 +13,23 @@ function addOptinalInfo() {
         }
     });
 }
+
+function addTree() {
+    var name = document.getElementById('addTree_Name').value;
+    var ID = document.getElementById('addTree_ID').value;
+    var location = document.getElementById('addTree_Location').value;
+    var datetimeStart = document.getElementById('addTree_dateStart').value;
+    var datetimeEnd = document.getElementById('addTree_dateEnd').value;
+    var getURL = "http://" + ip + ":" + port + "/DATN/addTree.php?name=" + name + "&ID=" + ID + "&location=";
+    getURL += location + "&datetimeStart=" + datetimeStart + "&datetimeEnd=" + datetimeEnd;
+    $.ajax({
+        url: getURL,
+        type: "GET",
+        success: function(data) {
+            alert("them thong tin thanh cong");
+        },
+        error: function(data) {
+            alert("ko co ket noi toi database");
+        }
+    });
+}
