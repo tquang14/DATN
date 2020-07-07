6b9c8e0a4bc00e026df823b0c0be026c784f963e
tquang14 <tquang14@users.noreply.github.com>
2020-07-07 16:05:17 +0700

    back to control throught firebase

---
+++ web_dieuKhien_chiCoUI/js/firebase/firebaseInit.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAM7sR0yAVuI_dz4ob40mvsgpqGfYdL-cQ",
+    authDomain: "smart-garden-quynh-quang.firebaseapp.com",
+    databaseURL: "https://smart-garden-quynh-quang.firebaseio.com",
+    projectId: "smart-garden-quynh-quang",
+    storageBucket: "smart-garden-quynh-quang.appspot.com",
+    messagingSenderId: "290698463445",
+    appId: "1:290698463445:web:a981a11ebcdf546a5eba5c"
+};
+
+firebase.initializeApp(firebaseConfig);
