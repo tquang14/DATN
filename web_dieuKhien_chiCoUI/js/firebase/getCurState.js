3f53721116e1424e53913eed6594a2e6dd5a23c4
tquang14 <tquang14@users.noreply.github.com>
2020-07-09 22:53:24 +0700

    working on auto mode

--- web_dieuKhien_chiCoUI/js/firebase/getCurState.js
+++ web_dieuKhien_chiCoUI/js/firebase/getCurState.js
@@ -50,6 +50,7 @@ ID.on('value', function(snapshot) {
     drawQRcode();
 });
 
+// get current mode
 var autoMode = firebase.database().ref('mode');
 autoMode.on('value', function(snapshot) {
     console.log(snapshot.val());
@@ -57,11 +58,41 @@ autoMode.on('value', function(snapshot) {
         mode = 'auto';
         document.getElementById('icon-auto').style.filter = 'grayscale(0%)';
         $('#control').fadeOut(500);
+        $('#set-auto').fadeIn(500);
     } else if (snapshot.val() === "manual") {
         mode = 'manual';
         document.getElementById('icon-auto').style.filter = 'grayscale(100%)';
         $('#control').fadeIn(500);
+        $('#set-auto').fadeOut(500);
     } else {
         alert('ko phat hien mode auto/manual');
     }
 });
+
+// get auto value in auto mode
+const autoRefs = ["nhietdo_kk", "doam_kk", "doam_dat"];
+const autoDivIDs = ["temperatureRangeSlider", "humidityRangeSlider", "SMRangeSlider"];
+// this used for jquery to lazy to use '# + autoDivIDs' :v
+const RangeSliderIDs = ["#temperatureRangeSlider", "#humidityRangeSlider", "#SMRangeSlider"];
+for (let index = 0; index < autoRefs.length; index++) {
+    const autoRef = autoRefs[index];
+    var sensorVal = firebase.database().ref('value_sensor/' + autoRef);
+    sensorVal.once('value', function(snapshot) {
+        let autoDivID = document.getElementById(autoDivIDs[index]);
+        autoDivID.value = parseFloat(snapshot.val());
+        // change range slider UI
+        var rangePercent = $(RangeSliderIDs[index]).val();
+        if (index == 0) {
+            $('#rangePercent').html(rangePercent + '<span></span>');
+            $(RangeSliderIDs[index] + ', h4>span').css('filter', 'hue-rotate(-' + rangePercent / 2 + 'deg)');
+            // $('h4').css({'transform': 'translateX(calc(-50% - 20px)) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
+            $('#rangePercent').css({ 'transform': 'translateX(-50%) scale(' + (1 + (rangePercent / 140)) + ')', 'left': rangePercent + '%' });
+        } else {
+            $('#rangePercent' + (index).toString()).html(rangePercent + '<span></span>');
+            $(RangeSliderIDs[index] + ', h4>span').css('filter', 'hue-rotate(-' + rangePercent / 2 + 'deg)');
+            // $('h4').css({'transform': 'translateX(calc(-50% - 20px)) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
+            $('#rangePercent' + (index).toString()).css({ 'transform': 'translateX(-50%) scale(' + (1 + (rangePercent / 140)) + ')', 'left': rangePercent + '%' });
+        }
+
+    });
+}
